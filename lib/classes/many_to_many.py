7084c055189a2b43a6c1918b310e8d647865302d
Ahakim-xi <ahakimiii398@gmail.com>
2025-08-20 16:06:07 +0300

    code fix

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,88 +1,135 @@
-class Author:
-    
-    def __init__(self, name):
-        if not isinstance(name, str) or len(name.strip()) == 0:
-            raise Exception("Name must be a non-empty string")
-        self.name = name.strip()
-
-    def __repr__(self):
-        return f"Author({self.name})"
-
-
-class Magazine:
-    def __init__(self, name, category):
-        self.name = name
-        self.category = category
-
-    @property
-    def name(self):
-        return self._name
-
-    @name.setter
-    def name(self, value):
-        if not isinstance(value, str):
-            raise Exception("Name must be a string")
-        if not (2 <= len(value) <= 16):
-            raise Exception("Name must be between 2 and 16 characters")
-        self._name = value
-
-    @property
-    def category(self):
-        return self._category
-
-    @category.setter
-    def category(self, value):
-        if not isinstance(value, str):
-            raise Exception("Category must be a string")
-        if len(value.strip()) == 0:
-            raise Exception("Category cannot be empty")
-        self._category = value
-
-    def __repr__(self):
-        return f"Magazine({self.name}, {self.category})"
-
-
 class Article:
     all = []
 
     def __init__(self, author, magazine, title):
         if not isinstance(author, Author):
-            raise Exception("Author must be an Author instance")
+            raise TypeError("author must be an instance of Author")
         if not isinstance(magazine, Magazine):
-            raise Exception("Magazine must be a Magazine instance")
-        if not isinstance(title, str):
-            raise Exception("Title must be a string")
-        if not (5 <= len(title) <= 50):
-            raise Exception("Title must be between 5 and 50 characters")
-
-        self.author = author
-        self.magazine = magazine
+            raise TypeError("magazine must be an instance of Magazine")
+        if not isinstance (title, str):
+            raise TypeError("title must be a string")
+        if not(5 <= len(title) <= 50):
+            raise ValueError("title must be between 5 and 50 characters long")
+        
         self._title = title
+        self._author = author
+        self._magazine = magazine
+        
 
         Article.all.append(self)
 
     @property
     def title(self):
-        return self._title  # immutable (no setter)
+        return self._title
+    
+    
+    @property 
+    def author(self):
+        return self._author
+    
+    @property
+    def magazine(self):
+        return self._magazine
+    
 
-    def __repr__(self):
-        return f"Article({self.title}, {self.author}, {self.magazine})"
+class Author:
+    def __init__(self, name):
+        if not isinstance(name,str):
+            raise TypeError("name must be a string")
+        if len(name) == 0:
+            raise ValueError("name cannot be empty")
+        self._name = name
 
-    @classmethod
-    def articles(cls):
-        return cls.all
+    @property
+    def name(self):
+        return self._name
+    
+    def articles(self):
+        return [article for article in Article.all if article.author == self]
 
-    @classmethod
-    def contributors(cls):
-        return list(set(article.author for article in cls.all))
+    def magazines(self):
+        return list({article.magazine for article in self.articles()})
 
-    @classmethod
-    def article_titles(cls):
-        return [article.title for article in cls.all]
+    def add_article(self, magazine, title):
+        return Article(self, magazine, title)
 
-    @classmethod
-    def contributing_authors(cls):
-        authors = {}
-        for article in cls.all:
-            authors[article.author] = authors.get(article.author, 0) + 1
-        return [author for author, count in authors.items() if count > 2]
+    def topic_areas(self):
+        if not self.articles():
+         return None
+        return list({article.magazine.category for article in self.articles()})
+
+class Magazine:
+    all = []
+
+    def __init__(self, name, category):
+        self.name = name
+        self.category = category
+        Magazine.all.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+    
+
+    @name.setter
+    def name(self, magazine_name):
+        if not isinstance(magazine_name, str):
+            raise TypeError("Name must be a string")
+        if not (2 <= len(magazine_name) <= 16):
+            raise ValueError("Name must be between 2 and 16 characters long")
+        self.__name = magazine_name
+
+    @property
+    def category(self):
+        return self.__category
+    
+    @category.setter
+    def category(self, value):
+        if not isinstance(value, str):
+            raise TypeError("category must be a string")
+        if len(value) == 0:
+            raise ValueError("category cannot be empty")
+        self.__category = value
+
+    def articles(self):
+        return [article for article in Article.all if article.magazine == self]
+
+    def contributors(self):
+        return list({article.author for article in self.articles()})
+
+    def article_titles(self):
+      titles = [article.title for article in self.articles()]
+      return titles if titles else None
+    
+    def contributing_authors(self):
+        author_counts = {}
+        for article in self.articles():
+            author_counts[article.author] = author_counts.get(article.author, 0) + 1
+
+        result = [author for author, count in author_counts.items() if count > 2]
+        return result if result else None
+#create authors
+author1 = Author("Mark Twain")
+author2 = Author("Jane Chebet")
+
+ #create magazines
+magazine1 = Magazine("Literary Digest", "Literature")
+magazine2 = Magazine("Science Monthly", "Science")
+
+
+#articles
+article1 = author1.add_article(magazine1, "The Adventures of Tom Sawyer")
+article2 = author2.add_article(magazine1, "Pride and Prejudice")
+article3 = author1.add_article(magazine2, "A Connecticut Yankee in King Arthur's Court")
+article4 = author2.add_article(magazine2, "Future Visions of Science")
+
+# Print all articles
+print("All articles:", [a.title for a in Article.all])
+print("Articles in Literary Digest:", magazine1.article_titles())
+print("Contributors to Literary Digest:", [a.name for a in magazine1.contributors()])
+print("Mark Twain's magazines:", [m.name for m in author1.magazines()])
+print("Mark Twain's topic areas:", author1.topic_areas())
+
+
+    
+    
